8f29878e22506ccd74e2d06f74d4b8ca3a0293d0
MarcWong <brucewayne@pku.edu.cn>
2016-02-14 10:54:23 +0800

    1

--- upload_file.php
+++ upload_file.php
@@ -9,6 +9,8 @@ else
   move_uploaded_file($_FILES['file']['tmp_name'],$filename);
   chmod($filename, 0777);
 echo "haha";
-$a = exec("opencv.exe");  
+$a = exec("opencv.exe");
+if($a==0)echo "opencv.exe successfully executed";
+else "opencv.exe haven't been executed";
   }
 ?>
